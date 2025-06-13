611400a76c4c747abb6003de7eba5fbdadd0fb3e
Louis Dierickx <dierickx.louis01@gmail.com>
2025-06-13 14:08:59 +0200

    d

--- server/app.ts
+++ server/app.ts
@@ -9,24 +9,16 @@ const PORT: number = parseInt(<string>process.env.PORT, 10) || 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-
 app.use(expressLayouts);
 app.set("layout", "layouts/main");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.use(express.json()); // Add this line to parse JSON bodies
-app.use(express.urlencoded({ extended: true })); // For form data
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", routes);
 
-
-// Export the app for serverless environments (like Vercel)
-export default app;
-
-// Only start the server if not in a serverless environment
-if (require.main === module) {
-  app.listen(PORT, (): void => {
-    console.log(`Server draait op http://localhost:${PORT}`);
-  });
-}
+// Server starten
+app.listen(PORT, (): void => {
+  console.log(`Server draait op http://localhost:${PORT}`);
+});
